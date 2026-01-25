50e291d2f75fa6f630b99d6507d427fc04c348c3
Marko Vukas <marko.vukas4@gmail.com>
2026-01-25 18:48:38 +0100

    domaći

---
+++ Bibliotečni informacijski Sustav .Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUnosKnjige = new System.Windows.Forms.Button();
+            this.btnPrikaz = new System.Windows.Forms.Button();
+            this.btnstatistika = new System.Windows.Forms.Button();
+            this.btnIzlaz = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUnosKnjige
+            // 
+            this.btnUnosKnjige.Location = new System.Drawing.Point(44, 12);
+            this.btnUnosKnjige.Name = "btnUnosKnjige";
+            this.btnUnosKnjige.Size = new System.Drawing.Size(135, 43);
+            this.btnUnosKnjige.TabIndex = 0;
+            this.btnUnosKnjige.Text = "Unos knjige";
+            this.btnUnosKnjige.UseVisualStyleBackColor = true;
+            // 
+            // btnPrikaz
+            // 
+            this.btnPrikaz.Location = new System.Drawing.Point(44, 75);
+            this.btnPrikaz.Name = "btnPrikaz";
+            this.btnPrikaz.Size = new System.Drawing.Size(135, 46);
+            this.btnPrikaz.TabIndex = 1;
+            this.btnPrikaz.Text = "Prikaz knjiga";
+            this.btnPrikaz.UseVisualStyleBackColor = true;
+            // 
+            // btnstatistika
+            // 
+            this.btnstatistika.Location = new System.Drawing.Point(44, 136);
+            this.btnstatistika.Name = "btnstatistika";
+            this.btnstatistika.Size = new System.Drawing.Size(135, 43);
+            this.btnstatistika.TabIndex = 2;
+            this.btnstatistika.Text = "Statistika žanrova";
+            this.btnstatistika.UseVisualStyleBackColor = true;
+            // 
+            // btnIzlaz
+            // 
+            this.btnIzlaz.Location = new System.Drawing.Point(44, 194);
+            this.btnIzlaz.Name = "btnIzlaz";
+            this.btnIzlaz.Size = new System.Drawing.Size(135, 43);
+            this.btnIzlaz.TabIndex = 3;
+            this.btnIzlaz.Text = "Izlaz";
+            this.btnIzlaz.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btnIzlaz);
+            this.Controls.Add(this.btnstatistika);
+            this.Controls.Add(this.btnPrikaz);
+            this.Controls.Add(this.btnUnosKnjige);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUnosKnjige;
+        private System.Windows.Forms.Button btnPrikaz;
+        private System.Windows.Forms.Button btnstatistika;
+        private System.Windows.Forms.Button btnIzlaz;
+    }
+}
+
---
+++ Unos knjige.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Unos_knjige
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNaslov = new System.Windows.Forms.TextBox();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.labelNaslov = new System.Windows.Forms.Label();
+            this.labelAutor = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNaslov
+            // 
+            this.txtNaslov.Location = new System.Drawing.Point(71, 15);
+            this.txtNaslov.Name = "txtNaslov";
+            this.txtNaslov.Size = new System.Drawing.Size(226, 22);
+            this.txtNaslov.TabIndex = 0;
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(96, 52);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(201, 22);
+            this.txtAutor.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // labelNaslov
+            // 
+            this.labelNaslov.AutoSize = true;
+            this.labelNaslov.Location = new System.Drawing.Point(12, 18);
+            this.labelNaslov.Name = "labelNaslov";
+            this.labelNaslov.Size = new System.Drawing.Size(53, 16);
+            this.labelNaslov.TabIndex = 3;
+            this.labelNaslov.Text = "Naslov:";
+            // 
+            // labelAutor
+            // 
+            this.labelAutor.AutoSize = true;
+            this.labelAutor.Location = new System.Drawing.Point(24, 58);
+            this.labelAutor.Name = "labelAutor";
+            this.labelAutor.Size = new System.Drawing.Size(41, 16);
+            this.labelAutor.TabIndex = 4;
+            this.labelAutor.Text = "Autor:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(348, 50);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(153, 22);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(33, 71);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(160, 20);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Dostupna za posudbu";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(87, 41);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Broj stranica:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(308, 143);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(219, 110);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dodatni podatci";
+            // 
+            // Unos_knjige
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.labelAutor);
+            this.Controls.Add(this.labelNaslov);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.txtNaslov);
+            this.Name = "Unos_knjige";
+            this.Text = "Unos_knjige";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNaslov;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label labelNaslov;
+        private System.Windows.Forms.Label labelAutor;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
